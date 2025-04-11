50f0756cc64fa08beac1972299c151e565fd5ab5
Jacob Schwartz <jacob@jaschwartz.net>
2025-04-11 09:52:14 -0400

    Add more tests

--- test/test_one.py
+++ test/test_one.py
@@ -5,32 +5,65 @@ from pytest import approx
 π = np.pi
 
 # default values
-pp = 1.0
-zz = 2.0
-rr = 0.5
-ff = φφ = 0.6
+p = 1.0
+z = 2.0
+r = 0.5
+f = φ = 0.6
+
+α = 0.3
+β = 1.3
+
+high_accuracy = 4e-16
 
 def test_g_HI():
-    g.g_HI(p=pp, z=zz, r=rr, φ=ff)
+    g.g_HI(p=p, z=z, r=r, φ=f)
 
 def test_g_HI_0():
-    ans = g.g_HI(p=pp, z=zz, r=rr, φ=0)
+    ans = g.g_HI(p=p, z=z, r=r, φ=0)
     assert ans == 0
 
 def test_g_HI_pi():
-    ans = g.g_HI(p=pp, z=zz, r=rr, φ=π)
+    ans = g.g_HI(p=p, z=z, r=r, φ=π)
 
 def test_g_HI_symmetry():
-    ans_1 = g.g_HI(p=pp, z=zz, r=rr, φ=ff)
-    ans_2 = g.g_HI(p=pp, z=-zz, r=rr, φ=ff)
+    ans_1 = g.g_HI(p=p, z=z, r=r, φ=f)
+    ans_2 = g.g_HI(p=p, z=-z, r=r, φ=f)
     assert ans_1 == ans_2
 
 def test_g_HI_same_height():
-    ans_1 = g.g_HI(p=pp, z=0, r=rr, φ=ff)
-    ans_2 = g.g_HI_same_height(p=pp, r=rr, φ=ff)
+    ans_1 = g.g_HI(p=p, z=0, r=r, φ=f)
+    ans_2 = g.g_HI_same_height(p=p, r=r, φ=f)
     assert ans_1 == ans_2
 
 def test_g_HA():
-    ans = g.g_HA(p=pp, z=zz, r=rr, φ=ff)
+    ans = g.g_HA(p=p, z=z, r=r, φ=f)
     mathematica_answer = 0.058981191793359559063
-    assert ans == approx(mathematica_answer, abs=1e-16)
+    assert ans == approx(mathematica_answer, abs=high_accuracy)
+
+# test that sums of A and cos equal the isotropic distributions
+def test_g_HI_sum():
+    iso = g.g_HI(p=p, z=z, r=r, φ=f)
+    a = g.g_HA(p=p, z=z, r=r, φ=f)
+    c = g.g_Hc(p=p, z=z, r=r, φ=f)
+    assert iso == approx(a + c, abs=high_accuracy)
+
+def test_g_VI_sum():
+    iso = g.g_VI(p=p, z=z, r=r, φ=f)
+    a = g.g_VAa(p=p, z=z, r=r, α=α, β=β, φ=f)
+    c = g.g_Vca(p=p, z=z, r=r, α=α, β=β, φ=f)
+    assert iso == approx(a + c, abs=high_accuracy)
+
+# test relationship between g_HI, g_HBa and g_Hca
+def test_g_HB_sum():
+    b = g.g_HBa(p=p, z=z, r=r, α=α, β=β, φ=f)
+    iso = g.g_HI(p=p, z=z, r=r, φ=f)
+    c = g.g_Hca(p=p, z=z, r=r, α=α, β=β, φ=f)
+    assert b == approx(iso/4 + 3*c/4, abs=high_accuracy)
+
+# test relationship between g_VI, g_VBa and g_Vca
+def test_g_VB_sum():
+    b = g.g_VBa(p=p, z=z, r=r, α=α, β=β, φ=f)
+    iso = g.g_VI(p=p, z=z, r=r, φ=f)
+    c = g.g_Vca(p=p, z=z, r=r, α=α, β=β, φ=f)
+    assert b == approx(iso/4 + 3*c/4, abs=high_accuracy)
+
