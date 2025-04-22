a89da8c58d8bd7574bcc0bc42d21e5f73e82d961
Jacob Schwartz <jacob@jaschwartz.net>
2025-04-21 23:47:52 -0400

    Add my function implementaitons

--- src/anarrima/elliptic/carlson.py
+++ src/anarrima/elliptic/carlson.py
@@ -1,44 +1,105 @@
+import jax
 import jax.numpy as jnp
 from jax.lax import scan
 
 sqrt = jnp.sqrt
 
-NUM_LOOPS = 9
-def rf(x, y, z):
+@jax.custom_jvp
+def rf(x0, y0, z0):
     """
     """
-   
-    xyz = jnp.array([x, y, z])
-    A0 = jnp.sum(xyz) / 3
-    init = A0, xyz
-    
+
+    NUM_LOOPS = 6
+
+    v0 = jnp.array([x0, y0, z0])
+    A0 = jnp.sum(v0) / 3
+    init = A0, v0
+
     def body(carry, _):
-        An, xyz = carry
-        λ = sqrt(xyz[0]*xyz[1]) + sqrt(xyz[0]*xyz[2]) + sqrt(xyz[1]*xyz[2])
+        Am, vm = carry
+        λ = sqrt(vm[0]*vm[1]) + sqrt(vm[0]*vm[2]) + sqrt(vm[1]*vm[2])
 
-        An_new = (An + λ) / 4
-        xyz_new = (xyz + λ) / 4
+        Am_new = (Am + λ) / 4
+        vm_new = (vm + λ) / 4
 
-        return (An_new, xyz_new), None
+        return (Am_new, vm_new), None
 
     result, _ = scan(body, init, length=NUM_LOOPS)
     an, _ = result
-    
+
     f = 4**(-NUM_LOOPS)
 
-    x = (A0 - x) / an * f
-    y = (A0 - y) / an * f
+    x = (A0 - x0) / an * f
+    y = (A0 - y0) / an * f
     z = -(x + y)
-    E2 = x * y - z * z
-    E3 = x * y * z
+    e2 = x * y - z * z
+    e3 = x * y * z
 
     return (
-        1 
-        - E2 / 10
-        + E3 / 14 
-        + E2 * E2 / 24 
-        - 3 * E2 * E3 / 44
+        1
+        - e2 / 10
+        + e3 / 14
+        + e2 * e2 / 24
+        - 3 * e2 * e3 / 44
+        + (5 * e2**3 / 208
+        + 3 * e3**2 / 104)
     ) / sqrt(an)
 
-def rd(x, y, z):
-    pass
+
+def rd(x0, y0, z0):
+    """
+    """
+
+    NUM_LOOPS=6
+
+    v0 = jnp.array([x0, y0, z0])
+    A0 = (x0 + y0 + 3*z0) / 5
+    init = A0, v0
+
+    def body(carry, numer):
+        Am, vm = carry
+        xm, ym, zm = vm
+
+        r_xm, r_ym, r_zm = sqrt(xm), sqrt(ym), sqrt(zm)
+        λ = r_xm * r_ym + r_ym * r_zm + r_zm * r_xm
+
+        Am_new = (Am + λ) / 4
+        vm_new = (vm + λ) / 4
+
+        denom = r_zm * (zm + λ)
+
+        sum_element = numer / denom
+
+        return (Am_new, vm_new), sum_element
+
+    numerators = jnp.power(4., -jnp.arange(NUM_LOOPS))
+
+    result, sum_elements = scan(body, init, xs=numerators, length=NUM_LOOPS)
+    an, _ = result
+
+    scale = numerators[-1]/4
+
+    x = (A0 - x0) / an * scale
+    y = (A0 - y0) / an * scale
+    z = -(x + y) / 3
+
+    e2 = x * y - 6 * z**2
+    e3 = (3 * x * y - 8 * z**2) * z
+    e4 = 3 * (x * y - z**2) * z**2
+    e5 = x * y * z**3
+
+    c1, c2, c3 = -3/14, 1/6, 9/88
+    c4, c5, c6 = -3/22, -9/52, 3/26
+
+    series = (1 + c1 * e2 + c2 * e3
+                + c3 * e2**2 + c4 * e4
+                + c5 * e2 * e3 + c6 * e5)
+    series_term = scale * series / (an * sqrt(an))
+
+    sum_term = 3 * jnp.sum(sum_elements)
+
+    return sum_term + series_term
+
+rf.defjvps(lambda x_dot, primal_out, x, y, z: -rd(y, z, x)/6 * x_dot,
+           lambda y_dot, primal_out, x, y, z: -rd(z, x, y)/6 * y_dot,
+           lambda z_dot, primal_out, x, y, z: -rd(x, y, z)/6 * z_dot)
--- src/anarrima/elliptic/legendre.py
+++ src/anarrima/elliptic/legendre.py
@@ -1,7 +1,20 @@
 from anarrima.elliptic.carlson import rf, rd
+import jax.numpy as jnp
 
 def ellipeinc(φ, m):
-    pass
+    sinφ = jnp.sin(φ)
+    cosφ = jnp.cos(φ)
+    sin_sq_φ = sinφ * sinφ
+    sin_cu_φ = sin_sq_φ * sinφ
+    x = cosφ * cosφ
+    y = 1. - m * sin_sq_φ
+    z = 1.
+    return sinφ * rf(x, y, z) - m * sin_cu_φ * rd(x, y, z) / 3
 
 def ellipfinc(φ, m):
-    
+    sinφ = jnp.sin(φ)
+    cosφ = jnp.cos(φ)
+    x = cosφ * cosφ
+    y = 1. - m * sinφ * sinφ
+    z = 1.
+    return sinφ * rf(x, y, z)
