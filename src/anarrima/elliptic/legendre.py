bfa73ed54ec19e24063b845b4f5ab1dbb000d925
Jacob Schwartz <jacob@jaschwartz.net>
2025-04-24 16:55:52 -0400

    Add special cases to legendre K

--- src/anarrima/elliptic/legendre.py
+++ src/anarrima/elliptic/legendre.py
@@ -80,15 +80,83 @@ def ellipeinc_jvp(primals, tangents):
 # Treatment of Legendre K(m)
 ##############################
 
-def k(m):
+def ellipk(m):
     """Legendre K"""
-    use_standard = m > -1e10
-    standard_eval = elliprf(0., 1. - m, 1.)
-    # backup series for large, negative m
-    large_neg_series_eval = _k_series_large_negative_2_terms(m)
-    return jnp.where(use_standard, standard_eval, large_neg_series_eval)
+    above_large_negative_range = m > -1e10
+    below_0 = m < 0
+    below_1 = m < 1
 
-##### m very close to 1
+    is_finite = jnp.isfinite(m)
+
+    # case_0
+    is_neginf = jnp.isneginf(m)
+
+    # case_1
+    use_large_negative = jnp.logical_and(is_finite, jnp.logical_not(above_large_negative_range))
+
+    # case_2
+    use_standard = jnp.logical_and(above_large_negative_range, below_1)
+
+    # case 3
+    is_unity = m == 1.0
+
+    # case 4
+    is_more_than_1 = m > 1
+
+    # case_5
+    is_nan = jnp.isnan(m)
+
+    zero_integer = jnp.zeros_like(m, dtype=jnp.int8)
+    which = (zero_integer + 
+             1 * use_large_negative +
+             2 * use_standard +
+             3 * is_unity +
+             4 * jnp.logical_or(is_more_than_1, is_nan))
+
+    ### evaluate using Carlson's Rf
+    # the 0.0 at the end is a safe value for K
+    sanitized_m = jnp.where(jnp.logical_and(is_finite, below_1), m, 0.)
+    standard_eval = _ellipk(sanitized_m)
+
+    ### series for large, negative m
+    # here the -1 is a safe value for the series treatment, which contains log(-m).
+    sanitized_m2 = jnp.where(jnp.logical_and(is_finite, below_0), m, -1.)
+    large_neg_series_eval = _k_series_large_negative_2_terms(sanitized_m2)
+
+    ### outputs for special cases:
+    # K(-inf) == 0
+    zero = jnp.zeros_like(m)
+    # K(1) == inf
+    infty = jnp.full_like(m, jnp.inf)
+    # K(x > 1) == nan
+    nans = jnp.full_like(m, jnp.nan)
+
+    return jax.lax.select_n(which,
+                            zero,
+                            large_neg_series_eval,
+                            standard_eval,
+                            infty,
+                            nans)
+
+# good from -1e10 < m < 1
+def _ellipk(m):
+    return elliprf(0., 1. - m, 1.)
+
+# good for m < -1e10
+def _k_series_large_negative_2_terms(m):
+    w = -m # wumbo
+    ln2 = jnp.log(2)
+    lnw = jnp.log(w)
+    root_w = jnp.sqrt(w)
+    term_0 = (4 * ln2 + lnw)/(2 * root_w)
+    term_1 = (2 - 4 * ln2 - lnw) / (8 * root_w * w)
+    return term_0 + term_1
+
+# m very close to 1
+# this is not used because with the current number of loop
+# evaluations in the rf function it is good enough, even down 
+# to 1e-16 ish. So even doing  p = 1.0 - m loses precision enough
+# to not make it worth it.
 def _km1_series_6_terms(p):
     # within 1e-16 for p < 1e-3
     # p = 1 - m
@@ -102,12 +170,3 @@ def _km1_series_6_terms(p):
     term_5 = 63 * p**5 * (2520 * ln2 - 630 * lnp - 1627) / 1310720
     return term_0 + (term_1 + (term_2 + (term_3 + (term_4 + term_5))))
 
-# good for m < -1e10
-def _k_series_large_negative_2_terms(m):
-    w = -m # wumbo
-    ln2 = jnp.log(2)
-    lnw = jnp.log(w)
-    root_w = jnp.sqrt(w)
-    term_0 = (4 * ln2 + lnw)/(2 * root_w)
-    term_1 = (2 - 4 * ln2 - lnw) / (8 * root_w * w)
-    return term_0 + term_1
