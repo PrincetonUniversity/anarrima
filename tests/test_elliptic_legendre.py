d7f563a0f9166c35bfc9f9aa3daf81e2a72d96be
Jacob Schwartz <jacob@jaschwartz.net>
2025-04-28 20:07:08 -0400

    Add legendre integral tests

---
+++ tests/test_elliptic_legendre.py
@@ -0,0 +1,49 @@
+import anarrima.elliptic.legendre as legendre
+import pytest
+from pytest import approx
+import jax.numpy as jnp
+
+INF = jnp.inf
+isneginf = jnp.isneginf
+
+einc = legendre.ellipeinc
+finc = legendre.ellipfinc
+
+# Test RF
+φ0 = 0.5
+m0 = -1.0
+
+def test_fused_form_equality():
+    f1 = finc(φ0, m0)
+    e1 = einc(φ0, m0)
+    f2, e2 = legendre.ellip_finc_einc_fused(φ0, m0)
+    assert f1 == f2
+    assert e1 == e2
+
+# φ = 0, m < 0
+def test_ellipf_phi_zero():
+    f1 = finc(0., m0)
+    assert f1 == 0.
+
+def test_ellipe_phi_zero():
+    e1 = einc(0., m0)
+    assert e1 == 0.
+
+# φ = 0, m = -∞
+def test_ellipf_m_neginf():
+    f1 = finc(φ0, -INF)
+    assert f1 == 0.
+
+def test_ellipe_m_neginf():
+    e1 = einc(φ0, -INF)
+    assert isneginf(e1)
+
+# φ > 0, m = 0
+def test_ellipf_m_zero():
+    f1 = finc(φ0, 0.)
+    assert f1 == φ0
+
+def test_ellipe_m_zero():
+    e1 = einc(φ0, 0.)
+    assert e1 == φ0
+
