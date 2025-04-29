24838552c816703417444e89c982fa94828e58ac
Jacob Schwartz <jacob@jaschwartz.net>
2025-04-28 20:06:55 -0400

    Skip a Carlson integral test

--- tests/test_elliptic_carlson.py
+++ tests/test_elliptic_carlson.py
@@ -1,19 +1,21 @@
 import anarrima.elliptic.carlson as carlson
+import pytest
 from pytest import approx
-import jax.numpy as jnp
-jnp.set_printoptions(20)
+
+rf = carlson.rf
+rd = carlson.rd
 
 # Test RF
 x0 = 1.
 y0 = 2.
 z0 = 3.
 
+@pytest.mark.skip(reason="This cannot happen in anarrima geometry")
 def test_infinity_rf():
-    ans = carlson.rf(0., 0., 1.)
+    ans = rf(0., 0., 1.)
     assert jnp.isinf(ans)
 
 def test_permutations_rf():
-    rf = carlson.rf
     w0 = rf(x0, y0, z0)
     w1 = rf(y0, z0, x0)
     w2 = rf(z0, x0, y0)
@@ -22,7 +24,6 @@ def test_permutations_rf():
 
 # Test RD
 def test_permutations_rd():
-    rd = carlson.rd
     w0 = rd(x0, y0, z0)
     w1 = rd(y0, x0, z0)
     assert w0 == approx(w1, rel=2e-16)
